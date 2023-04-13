1bcebb5f353effae6fb574de23e549e93d9822bf
Librada Devers <libradastar@aol.com>
2023-04-12 18:44:42 -0700

    Lab Completed

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(hello) {
+    return hello.toUpperCase();
+}
+
+function whisper(hello) {
+    return hello.toLowerCase();
+}
+
+function logShout(hello) {
+    console.log(hello.toUpperCase());
+}
+
+function logWhisper(hello) {
+    console.log(hello.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === 'hello')
+    return"I can\'t hear you!"
+    else if (string === 'HELLO')
+    return"YES INDEED!"
+    else (string === 'let\'s have dinner together!')
+    return"I would love to!"
+}
